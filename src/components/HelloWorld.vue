dc3bd666bad9dde099240b3f01c3d7a84055e71d
Jack Koppa <hello@jackkoppa.com>
2020-05-16 18:17:53 -0400

    Demo type of Vue props code that fails w/ TS #38279

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ messages.join(' ') }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +35,10 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'HelloWorld',
   props: {
-    msg: String,
+    messages: {
+      type: Array,
+      default: () => []
+    }
   },
 });
 </script>
